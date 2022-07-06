82c40dd84fda90147d470cb52f9136b0874e0902
jujin1324 <jujin1324@daum.net>
2022-07-06 22:36:50 +0900

    Chapter3 - 리액터 로깅 및 블로킹 코드 검출을 위한 BlockHound 추가

--- src/main/java/starter/reactive/SpringReactiveStarterApplication.java
+++ src/main/java/starter/reactive/SpringReactiveStarterApplication.java
@@ -2,11 +2,17 @@ package starter.reactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.TemplateEngine;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class SpringReactiveStarterApplication {
 
     public static void main(String[] args) {
+        BlockHound.builder()
+                .allowBlockingCallsInside(TemplateEngine.class.getCanonicalName(), "process")
+                .install();
+
         SpringApplication.run(SpringReactiveStarterApplication.class, args);
     }
 
--- src/main/java/starter/reactive/domain/ecommerce/service/CartService.java
+++ src/main/java/starter/reactive/domain/ecommerce/service/CartService.java
@@ -27,21 +27,30 @@ public class CartService {
 
     public Mono<Void> addToCart(String cartId, String itemId) {
         return cartRepository.findById(cartId)
+                .log("foundCart")
                 .defaultIfEmpty(new Cart(cartId))
+                .log("emptyCart")
                 .flatMap(cart -> cart.getCartItems().stream()
                         .filter(cartItem -> cartItem.getItem().getId().equals(itemId))
                         .findAny()
                         .map(cartItem -> {
                             cartItem.increment();
-                            return Mono.just(cart);
+                            return Mono.just(cart)
+                                    .log("addedCartItem");
                         })
                         .orElseGet(() -> itemRepository.findById(itemId)
+                                .log("fetchedItem")
                                 .map(CartItem::new)
+                                .log("cartItem")
                                 .map(cartItem -> {
                                     cart.addCartItem(cartItem);
                                     return cart;
-                                })))
+                                })
+                                .log("newCartItem")
+                        ))
+                .log("cartWithAnotherItem")
                 .flatMap(this.cartRepository::save)
+                .log("savedCart")
                 .then();
     }
 }
