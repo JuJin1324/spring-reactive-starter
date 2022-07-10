663d9eee69d51a0b75b7772ae61d4dff37754d78
jujin1324 <jujin1324@daum.net>
2022-07-10 14:36:01 +0900

    Chapter5 - spring actuator: httptrace 추가

--- src/main/java/starter/reactive/SpringReactiveStarterApplication.java
+++ src/main/java/starter/reactive/SpringReactiveStarterApplication.java
@@ -1,13 +1,15 @@
 package starter.reactive;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.thymeleaf.TemplateEngine;
 import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class SpringReactiveStarterApplication {
-
     public static void main(String[] args) {
         BlockHound.builder()
                 .allowBlockingCallsInside(TemplateEngine.class.getCanonicalName(), "process")
@@ -16,4 +18,8 @@ public class SpringReactiveStarterApplication {
         SpringApplication.run(SpringReactiveStarterApplication.class, args);
     }
 
+    @Bean
+    HttpTraceRepository traceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
 }
